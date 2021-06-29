559a2f69962fdb750d3a97ca982db5cf6956a623
Manav <manavmohata123@gmail.com>
2021-06-29 15:36:19 +0530

    ADD: GRAB MOVIES FROM DB

--- src/App.js
+++ src/App.js
@@ -2,13 +2,35 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header'
 import Home from './components/Home'
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Detail from './components/Detail'
+import Login from './components/Login'
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path='/detail'>
+            <Detail />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+
+      </Router>
+
     </div>
   );
 }
